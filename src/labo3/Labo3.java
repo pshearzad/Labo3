74a11f80c41b4ce4149b4cfe9411f2494376bd40
MacNatch <MacNatch@172.29.103.170>
2016-09-26 11:03:23 -0400

    Test 1

--- src/labo3/Labo3.java
+++ src/labo3/Labo3.java
@@ -18,6 +18,7 @@ public class Labo3 {
         // TODO code application logic here
         System.out.println("Hello");
         System.out.println("test2");
+        System.out.println("conflit")
     }
     
 }
